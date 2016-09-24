e5a9536c52e6d5054a9376307101dfb114bb7db2
Mahmudur Rahman <nahidacm@gmail.com>
2016-09-24 12:44:40 +0600

    composer.json generation done

--- Commands/GenerateModule.php
+++ Commands/GenerateModule.php
@@ -20,6 +20,7 @@ class GenerateModule extends Command
     function execute()
     {
         $this->writeRegistrationFile();
+        $this->writeComposerFile();
 
         return "\n Done..";
     }
@@ -33,6 +34,23 @@ class GenerateModule extends Command
         $data = array(
             'package_name'=>$this->package_name,
             'module_name'=>$this->module_name
+        );
+
+        $output_file_contents = $this->helper->replacePlaceHolders($template_file_contents, $data);
+
+        $this->helper->writeCodeFile($output_file_path, $output_file_contents);
+    }
+    function writeComposerFile()
+    {
+        $template_file_path = ROOT.'templates/json/composer.tpl';
+        $output_file_path = $this->helper->getModuleBasePath().'composer.json';
+
+        $template_file_contents = file_get_contents($template_file_path);
+        $data = array(
+            'package_name'=>$this->package_name,
+            'module_name'=>$this->module_name,
+            'package_name_lc'=>strtolower($this->package_name),
+            'module_name_lc'=>strtolower($this->module_name)
         );
 
         $output_file_contents = $this->helper->replacePlaceHolders($template_file_contents, $data);
---
+++ templates/json/composer.tpl
@@ -0,0 +1,20 @@
+{
+    "name": "{{package_name_lc}}/{{module_name_lc}}",
+    "description": "",
+    "authors": [
+    {
+        "email": "nahid@magento2training.com",
+        "name": "Mahmudur Rahman"
+    }
+    ],
+    "minimum-stability": "dev",
+    "require": {},
+    "autoload": {
+        "files": [
+            "registration.php"
+        ],
+        "psr-4": {
+            "{{package_name}}\\{{module_name}}\\": ""
+        }
+    }
+}
